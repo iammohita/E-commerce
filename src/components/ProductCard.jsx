ad990c8cc96942cef51de2e59b449fd75775e1cc
MohiTa <MohiTa@gmail.com>
2023-02-13 16:55:44 +0330

    product added

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,32 +3,25 @@ import React, { Component } from "react";
 export default class ProductCard extends Component {
   render() {
     return (
-      <div>
-        <div className="bg-light w-25 p-3 border border-2 rounded">
+      <div className="d-flex flex-column w-25  m-4 border border-dark p-3">
+        <div className="mw-100 text-center">
+          <img src="https://picsum.photos/400" alt="random" />
+        </div>
+        <div className="d-flex justify-content-between mt-2 mb-3">
+          <div>نام محصول</div>
+          <div>لورم ایپسوم متن ساختگی با </div>
+        </div>
+        <div>
+          <h5 className="text-right">توضیحات</h5>
           <div>
-            <img
-              className="mh-25 w-100 d-inline-block"
-              src="https://picsum.photos/200"
-              alt="random image"
-            />
-            <div className="text-center">
-              <div className="d-flex justify-content-between m-3">
-                <span>نام محصول</span>
-                <span>لپتاپ ایسوس </span>
-              </div>
-              <div>
-                <h4>توضیحات</h4>
-              </div>
-              <div>
-                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-                استفاده از طراحان گرافیک است، چاپگرها و
-              </div>
-              <div className="d-flex justify-content-between m-4">
-                <span>قیمت</span>
-                <span> 12,200,000 </span>
-              </div>
-            </div>
+            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
+            استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
+            ستون و سطرآنچنان که{" "}
           </div>
+        </div>
+        <div className="d-flex justify-content-between mt-2">
+          <div>قیمت</div>
+          <div>2,220,000</div>
         </div>
       </div>
     );
