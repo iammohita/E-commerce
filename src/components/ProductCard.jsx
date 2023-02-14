af793a5324a5311b489d1350e145d6f9a989f6cb
MohiTa <MohiTa@gmail.com>
2023-02-14 16:31:24 +0330

    product card added(without bootstrap)

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,23 +3,27 @@ import React, { Component } from "react";
 export default class ProductCard extends Component {
   render() {
     return (
-      <div className="d-flex flex-column w-25  m-4 border border-dark p-3">
-        <div className="mw-100 text-center">
-          <img src="https://picsum.photos/400" alt="random" />
+      <div className="card-container">
+        <div className="card--image">
+          <img
+            className="mw-100"
+            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-card-40-iphone14pro-202209_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1663611329204"
+            alt="random"
+          />
         </div>
-        <div className="d-flex justify-content-between mt-2 mb-3">
-          <div>نام محصول</div>
-          <div>لورم ایپسوم متن ساختگی با </div>
+        <div className="card--name">
+          <div className="card--name">نام محصول</div>
+          <div className="card--name">لورم ایپسوم متن </div>
         </div>
         <div>
-          <h5 className="text-right">توضیحات</h5>
-          <div>
+          <h5 className="card--name">توضیحات</h5>
+          <div className="card--name--details">
             لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
             استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
             ستون و سطرآنچنان که{" "}
           </div>
         </div>
-        <div className="d-flex justify-content-between mt-2">
+        <div className="card--price">
           <div>قیمت</div>
           <div>2,220,000</div>
         </div>
