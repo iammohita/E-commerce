58741a6f407c6cdfcb1778931e3b3f3119dfef14
MohiTa <mohita1376@gmail.com>
2023-02-24 03:54:03 -0800

    Get Products' Data From Dummyjson

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,1 +1,21 @@
 // Your product-list will be rendered here
+import React, {useState,useEffect} from 'react'
+import axios from 'axios'
+
+const index = () => {
+    const [product, setProduct] = useState([]);
+    useEffect(()=>{
+        async function getProducts(){
+            const products = await axios.get("https://dummyjson.com/products");
+        console.log(products)
+        }
+        getProducts();
+    })
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default index
