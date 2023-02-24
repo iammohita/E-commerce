431512a1a711f5a9adb77ad2d99a3ad9405e1fd3
MohiTa <mohita1376@gmail.com>
2023-02-24 07:04:35 -0800

    product card lsited in gride style

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,20 +1,25 @@
 // Your product-list will be rendered here
 import  {useState,useEffect} from 'react'
 import axios from 'axios'
+import ProductCard from '../ProductCard';
 
-const [products, setProduct] = useState([]);
 const ProductList = () => {
+    const [products, setProducts] = useState([]);
     useEffect(()=>{
         async function getProducts(){
         const products = await axios.get("https://dummyjson.com/products");
-        setProduct(products.data)
-        }
-        getProducts();
+        setProducts(products.data.products)
+    }
+    getProducts();
     })
   return (
-    <div>
-    
+<div className='container'>
+    <div className="row">
+        {products.map(product=>(
+        <div key={product.id} className="col-lg"><ProductCard title={product.title} price={product.price} description= {product.description} image={product.images[0]}/></div>
+        ))}
     </div>
+</div>
   )
 }
 
