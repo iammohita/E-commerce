17e001fa7d47045e85f5216b12dfd24408de6eb5
MohiTa <mohita1376@gmail.com>
2023-02-24 07:15:57 -0800

    useState posittion corrected

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,8 +2,8 @@
 import  {useState,useEffect} from 'react'
 import axios from 'axios'
 
-const [products, setProduct] = useState([]);
 const ProductList = () => {
+    const [products, setProduct] = useState([]);
     useEffect(()=>{
         async function getProducts(){
         const products = await axios.get("https://dummyjson.com/products");
